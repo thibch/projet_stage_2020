b191ae050c17f84515acf53b9fc408bb7150d91e
Thibault Choné <43148335+thibch@users.noreply.github.com>
2020-04-06 11:05:55 +0200

    Initial commit

---
+++ desktop/src/fr/projetstage/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.projetstage.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.projetstage.ProjetStage;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjetStage(), config);
+	}
+}
